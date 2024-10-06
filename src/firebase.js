88117cbf4349718ac360ec2cc86bba8aa2b52ce9
superdividenz <75807121+superdividenz@users.noreply.github.com>
2024-10-05 23:03:25 -0500

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  // Replace with your Firebase configuration
-  apiKey: "AIzaSyBnQUOQxmYkQ2sW28QUpO7fxBqR9gFZkF0",
-  authDomain: "test-9776f.firebaseapp.com",
-  projectId: "test-9776f",
-  storageBucket: "test-9776f.appspot.com",
-  messagingSenderId: "744270644759",
-  appId: "1:744270644759:web:41d97039cfe8e1eb493e52"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
